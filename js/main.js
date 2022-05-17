772040b354158523c983f486b542374ad562f61d
Cassio Souza <cassio_souza@live.com>
2022-05-16 21:10:17 -0300

    Realizei algumas modificacoes no html, css, e js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,46 @@
-const selectForm = document.querySelector('#form');
-const selectInputNome = document.querySelector('#input-nome');
-const selectInputEmail = document.querySelector('#input-email');
-const selectInputtexto = document.querySelector('#texto');
+//Array dos dos input
+const pessoa = Array();
+pessoa[0] = document.getElementById('input-nome');
+pessoa[1] = document.getElementById('input-email');
+pessoa[2] = document.getElementById('input-texto');
 
+//Ação que envia formulario
+const enviarFormulario = () => {
+    capturarEvento()
+    validacaoFormulario()
 
+}
 
-function CriarPessoaFormulario(nome, email, texto) {
-    this.nome = nome;
-    this.email = email;
-    this.texto = texto;
+//Ação que valida os inputs do formulario
+const validacaoFormulario = () => {
+    const inputNomeRecuperado = pessoa[0].value;
+    const inputEmailRecuperado = pessoa[1].value;
+    const inputTextoRecuperado = pessoa[2].value;
+    //Validacao do nome
+    if (inputNomeRecuperado === '' ) {
+        alert('Campo inválido');
+        return false;
+    }
+    window.document.getElementById('input-nome').value = '';
+    //Validacao do email
+    if(inputEmailRecuperado.indexOf('@') === -1 || inputEmailRecuperado.indexOf('.com') === -1){
+        alert('Campo e-mail inválido, tento novamente');
+        pessoa[1].value = '';
+        return false;
+    } 
+    window.document.getElementById('input-email').value = '';
+    //Validacao da mensagem de texto
+    if(inputTextoRecuperado.length >= 100){
+        alert('Limite maximo de 100 caracters');
+        pessoa[2].value = '';
+        return false;
+    }
+    window.document.getElementById('input-texto').value = '';
+
 }
 
 
-selectForm.addEventListener('click', e => {
-    const el = e.target;
-    if (el.classList.contains('bnt-enviar-formulario')) {
-        
-        const p1 = new CriarPessoaFormulario(selectInputNome.value, selectInputEmail.value, selectInputtexto.value)
-        alert('Email enviado com sucesso')
-        
-    }
-
-})
+//Ação que captura os eventos
+const capturarEvento = () => {
+    return event.preventDefault()
+}
